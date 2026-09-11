c8f7001bbaa91b91f01b120358cb66f6ddf9c3dd
Muhammad Irfan Tijanie bin Mohammad Tasnim <adm_irfant@it-kl.dug.com>
2026-09-11 17:32:42 +0800

    move pdsh in relative to relogin

--- compal_logging.sh
+++ compal_logging.sh
@@ -2,7 +2,5 @@
 module load pdsh
 while true
 do
-	pdsh -g "office=kl&&H200" 'ps -eo ppid,pid,cmd | grep "[t]ail -F /var/log/messages" | awk "{print \$1}" | sort | uniq | while read line; do sudo kill -9 $line; sleep 1s; done' 
-	sleep 1s
-	sudo ls -1 /run/sol-capture/ | grep in | while read line; do echo $line; sudo printf 'root\n' | sudo tee /run/sol-capture/$line > /dev/null ; sleep 2s; sudo printf 'lend-virus-crushed\n' | sudo tee /run/sol-capture/$line > /dev/null; sleep 2s; sudo printf 'tail -F /var/log/messages &\n' | sudo tee /run/sol-capture/$line > /dev/null; done
+	sudo ls -1 /run/sol-capture/ | grep in | while read line; do echo $line;  NODE=$(echo $line | awk -F '.' '{print $1}'); pdsh -w "$NODE" 'ps -eo ppid,pid,cmd | grep "[t]ail -F /var/log/messages" | awk "{print \$1}" | sort | uniq | while read line; do sudo kill -9 $line; sleep 1s; done' ;sudo printf 'root\n' | sudo tee /run/sol-capture/$line > /dev/null ; sleep 2s; sudo printf 'lend-virus-crushed\n' | sudo tee /run/sol-capture/$line > /dev/null; sleep 2s; sudo printf 'tail -F /var/log/messages &\n' | sudo tee /run/sol-capture/$line > /dev/null; done
 done
---
+++ h200-telegrafsetup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+REPO=$(cat << 'EOF'
+[influxdata]
+name = InfluxData Repository - Stable
+baseurl = https://repos.influxdata.com/rhel/$releasever/$basearch/stable
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdata-archive_compat.key
+EOF
+)
+
+pdsh -g "H200&&office=kl" "echo -n | sudo tee /etc/yum.repos.d/influxdata.repo; echo '$REPO' | sudo tee /etc/yum.repos.d/influxdata.repo > /dev/null; sudo rpm --import https://repos.influxdata.com/influxdata-archive.key; sudo sed -i 's|influxdata-archive_compat.key|influxdata-archive.key|g' /etc/yum.repos.d/influxdata.repo; sudo dnf clean packages"
+
+CONFIG=$(cat << 'EOF'
+[agent]
+  interval = "10s"         # Reset agent default back to standard
+  round_interval = false
+  metric_batch_size = 1000
+  metric_buffer_limit = 10000
+  collection_jitter = "0s"
+  flush_interval = "1s"    # Flush metrics to InfluxDB immediately when collected
+  flush_jitter = "0s"
+  precision = "1s"
+  logfile = "/var/log/telegraf/telegraf.log"
+  logfile_rotation_max_size = "200MB"
+  logfile_rotation_max_archives = 5
+  log_with_timezone = "local"
+  hostname = ""
+  omit_hostname = false
+
+[[outputs.influxdb]]
+  urls = ["http://lustretest-grafana:8086"]
+  database = "lustretest"
+  retention_policy = "ninety_days"
+
+[[inputs.ipmi_sensor]]
+  path = "/bin/ipmitool"
+  use_sudo = true
+  use_cache = true         # Caches Sensor Data Records to speed up ipmitool execution
+  interval = "15s"         # Set to match actual ipmitool duration to eliminate log warnings
+  timeout = "60s"          # Prevents killing ipmitool during slower polling runs
+  metric_version = 2
+EOF
+)
+
+pdsh -g "H200&&office=kl" "sudo dnf remove telegraf -y; sudo dnf install telegraf -y; echo -n | sudo tee /etc/telegraf/telegraf.conf; echo '$CONFIG' | sudo tee /etc/telegraf/telegraf.conf > /dev/null && echo 'telegraf ALL=(ALL) NOPASSWD: /bin/ipmitool' | sudo tee /etc/sudoers.d/telegraf_ipmitool; sudo chmod 0440 /etc/sudoers.d/telegraf_ipmitool; sudo systemctl restart telegraf"
