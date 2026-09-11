8ba7832dd02cf76deaf6ee590b692b44ad4e560e
Muhammad Irfan Tijanie bin Mohammad Tasnim <adm_irfant@it-kl.dug.com>
2026-09-11 17:33:54 +0800

    create slurm_state_down script

---
+++ slurm_state_down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#monitor
+
+OLD_SLURM_STATE=""
+while true; do
+	CURRENT_SLURM_STATE=$(sinfo -p all -t down | sort -V)
+	if [[ "$OLD_SLURM_STATE" != "$CURRENT_SLURM_STATE" ]]; then
+		curl -d "$CURRENT_SLURM_STATE" ntfy.sh/kl_slurm_state
+		OLD_SLURM_STATE="$CURRENT_SLURM_STATE"
+	fi
+	sleep 5m
+done
